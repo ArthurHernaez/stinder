51d01867eb33bab1b04fb1e79648aff4220d5f9c
ArthurHernaez <ArthemuelHernaez@gmail.com>
2016-12-11 22:14:06 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$user = $_POST["user"];
+
+$pass = $_POST["pass"];
+
+
+if ($user == "stinder" && $pass == "123")
+
+	{echo "Welcome to Stinder";}
+
+
+ else {echo "incorrect login :(";} 
+		
+
+
+?>
